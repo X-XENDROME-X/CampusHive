fb9e6ba86beb8abddc035a86a74c22299161b0d3
XendromeJi <136829548+XendromeJi@users.noreply.github.com>
2024-10-07 12:57:32 -0700

    Created Instructor Home Page with Log Out Button

---
+++ Shorya/src/application/InstructorHomepageController.java
@@ -0,0 +1,23 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class InstructorHomepageController {
+
+    @FXML
+    private Label Heading;
+
+    @FXML
+    private Button LogOutButton;
+
+    @FXML
+    private ImageView Logo;
+
+}
+
+
+
