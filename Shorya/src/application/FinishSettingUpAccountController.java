41da4190e12e36e65eb8fe5cf8326b249b3b0207
XendromeJi <136829548+XendromeJi@users.noreply.github.com>
2024-10-07 12:07:41 -0700

    Creation of Finish Setting Up Account Page which includes Fist name, Last name, Middle Name, Email Address inputs with Save button and an Error Message Label

---
+++ Shorya/src/application/FinishSettingUpAccountController.java
@@ -0,0 +1,60 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class FinishSettingUpAccountController {
+
+    @FXML
+    private Label lastName;
+
+    @FXML
+    private TextField LastNameTextField;
+
+    @FXML
+    private TextField EmailAddressTestField;
+
+    @FXML
+    private Label emailAddress;
+
+    @FXML
+    private TextField FirstNameTestField;
+
+    @FXML
+    private Label firsName;
+
+    @FXML
+    private Label errorMessage;
+
+    @FXML
+    private Label middleName;
+
+    @FXML
+    private Button Save;
+
+    @FXML
+    private Label HeadingLabel;
+
+    @FXML
+    private TextField MiddleNameTextField;
+
+    @FXML
+    private ImageView Logo;
+
+    /*
+    @FXML
+    void c90909(ActionEvent event) {
+
+    }
+
+    @FXML
+    void 0171bc(ActionEvent event) {
+
+    }
+    */
+}
+
+
---
+++ Shorya/src/application/Main.java
@@ -0,0 +1,26 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Finish_Setting_Up_Page.fxml"));
+	        primaryStage.setTitle("Finish Setting Up Account");
+	        primaryStage.setScene(new Scene(root, 1920, 1080)); 
+	        primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
