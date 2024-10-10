b118c60c5d588abeb0fe2f07403f2013ecd3c2e7
Pranjal Shrvastava <pshriv365@gmail.com>
2024-10-09 23:39:18 -0700

    Update DatabaseInitializer.java

--- src/application/DatabaseInitializer.java
+++ src/application/DatabaseInitializer.java
@@ -1,3 +1,16 @@
+/*******
+ * <p> DatabaseInitializer Class </p>
+ * 
+ * <p> Description: This class is responsible for initializing the CSV-based database used in the Campus Hive application. It ensures that the necessary CSV files are created and ready for storing user-related data, handling the setup of the file structure and initial records if needed. This serves as a foundational component for ensuring smooth database operations within the application. </p>
+ * 
+ * <p> Copyright: Campus Hive Â© 2024 </p>
+ * 
+ * @author Th01 (Abhinav Ranish, Aditya Singh, Bharath Gowda, Pranjal Shrivastava, Shorya Raj)
+ * 
+ * @version 1.002024-10-09 First version of this JavaFX project was made
+ * 
+ */
+
 package application;
 
 import java.sql.Connection;
