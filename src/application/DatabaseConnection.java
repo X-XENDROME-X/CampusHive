61d69c9313d85746417196f2e9651fda2223fc45
Pranjal Shrvastava <pshriv365@gmail.com>
2024-10-09 23:38:30 -0700

    Update DatabaseConnection.java

--- src/application/DatabaseConnection.java
+++ src/application/DatabaseConnection.java
@@ -1,3 +1,16 @@
+/*******
+ * <p> DatabaseConnection Class </p>
+ * 
+ * <p> Description: This class handles the connection to the CSV-based storage system used in the Campus Hive application. It provides methods to interact with user data, such as adding, retrieving, and updating records stored in CSV format. The class ensures data persistence and facilitates communication between the application and the file-based storage system. </p>
+ * 
+ * <p> Copyright: Campus Hive Â© 2024 </p>
+ * 
+ * @author Th01 (Abhinav Ranish, Aditya Singh, Bharath Gowda, Pranjal Shrivastava, Shorya Raj)
+ * 
+ * @version 1.002024-10-09 First version of this JavaFX project was made
+ * 
+ */
+
 package application;
 
 import java.sql.Connection;
