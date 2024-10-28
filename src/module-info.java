e80e462cc05f6d7f1f00a384b6e8f49f6c8c2afe
XendromeJi <136829548+XendromeJi@users.noreply.github.com>
2024-10-27 20:24:38 -0700

    Refresh

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module CampusHive {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires java.mail;
-	requires java.sql;
-	requires java.activation;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
